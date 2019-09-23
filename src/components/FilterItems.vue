9412ff47787582d47c91ca182167104d973bb774
OsukeUesugi <funxion.mail@gmail.com>
2019-09-23 17:49:25 +0900

    moved logic from components to store

--- src/components/FilterItems.vue
+++ src/components/FilterItems.vue
@@ -3,27 +3,27 @@
     <v-col class="d-flex" cols="12" xl="6">
       <v-select
         v-model="pref01"
-        :items="prefs"
+        :items="$store.getters.pref"
         label="エリアを選択"
         hide-details
         outlined
+        @change="updatedArea"
       ></v-select>
     </v-col>
     <v-col v-if="pref01 === '東京'" class="d-flex" cols="12" xl="6">
       <v-select
         v-model="pref02"
-        :items="tokyo"
+        :items="$store.state.restaurant.tokyo"
         label="エリアを選択"
         hide-details
         outlined
+        @change="updatedArea"
       ></v-select>
     </v-col>
 
-    <v-col
-      v-if="(pref01.length > 0 && pref01 !== '東京') || (pref02.length > 0 && pref01 === '東京')"
-    >
+    <v-col>
       <FilterItem
-        v-for="restaurant in result"
+        v-for="restaurant in $store.state.restaurant.sortedItems"
         :key="`restaurant-${restaurant.url}`"
         :id="restaurant.id"
         :url="restaurant.url"
@@ -36,34 +36,6 @@
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import FilterItem from '@/components/FilterItem.vue';
-import { IRestaurant } from '@/api/fetchSakabaList';
-
-const tokyo: string[] = [
-  '千代田区',
-  '中央区',
-  '港区',
-  '新宿区',
-  '文京区',
-  '台東区',
-  '墨田区',
-  '江東区',
-  '品川区',
-  '目黒区',
-  '大田区',
-  '世田谷区',
-  '渋谷区',
-  '中野区',
-  '杉並区',
-  '豊島区',
-  '北区',
-  '荒川区',
-  '板橋区',
-  '練馬区',
-  '足立区',
-  '葛飾区',
-  '江戸川区',
-  '東京都（23区以外）',
-];
 
 @Component({
   components: {
@@ -71,7 +43,6 @@ const tokyo: string[] = [
   },
 })
 export default class FilterItems extends Vue {
-  private tokyo: string[] = tokyo;
   private pref01: string = '';
   private pref02: string = '';
 
@@ -81,36 +52,16 @@ export default class FilterItems extends Vue {
     }
   }
 
-  private get prefs() {
-    const areas: string[] = ['東京'];
+  private updatedArea() {
+    const area = {
+      pref01: this.pref01,
+      pref02: this.pref02,
+    };
 
-    this.$store.state.restaurant.items.forEach((restaurant: IRestaurant) => {
-      if (areas.indexOf(restaurant.area) === -1 && tokyo.indexOf(restaurant.area) === -1) {
-        areas.push(restaurant.area);
-      }
-    });
-
-    return areas;
-  }
-
-  private get result() {
-    let area: string = '';
-
-    if (this.pref01.length > 0 && this.pref01 !== '東京') {
-      area = this.pref01;
-    } else {
-      area = this.pref02;
-    }
-
-    const restaurants = this.$store.state.restaurant.items.filter((restaurant: IRestaurant) => {
-      return restaurant.area === area;
-    });
-
-    return restaurants;
+    this.$store.dispatch('setSortedItems', area);
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 </style>
--- src/store/index.ts
+++ src/store/index.ts
@@ -9,6 +9,33 @@ Vue.use(Vuex);
 @Module
 class Restaurant extends VuexModule {
   public items: IRestaurant[] = [];
+  public sortedItems: IRestaurant[] = [];
+  public tokyo: string[] = [
+    '千代田区',
+    '中央区',
+    '港区',
+    '新宿区',
+    '文京区',
+    '台東区',
+    '墨田区',
+    '江東区',
+    '品川区',
+    '目黒区',
+    '大田区',
+    '世田谷区',
+    '渋谷区',
+    '中野区',
+    '杉並区',
+    '豊島区',
+    '北区',
+    '荒川区',
+    '板橋区',
+    '練馬区',
+    '足立区',
+    '葛飾区',
+    '江戸川区',
+    '東京都（23区以外）',
+  ];
 
   @Action
   public async setItems() {
@@ -18,14 +45,49 @@ class Restaurant extends VuexModule {
     }
   }
 
+  @Action
+  public async setSortedItems(area: { pref01: string; pref02: string; } ) {
+    let selectedArea = '';
+
+    if (area.pref01.length > 0 && area.pref01 !== '東京') {
+      // not Tokyo
+      selectedArea = area.pref01;
+    } else {
+      // Tokyo
+      selectedArea = area.pref02;
+    }
+
+    const restaurants = this.items.filter((restaurant) => {
+      return restaurant.area === selectedArea;
+    });
+
+    this.context.commit('SET_SORTED_ITEMS', restaurants);
+  }
+
   @Mutation
   private SET_ITEMS(items: IRestaurant[]) {
     this.items = items;
   }
 
   @Mutation
+  private SET_SORTED_ITEMS(items: IRestaurant[]) {
+    this.sortedItems = items;
+  }
+
+  @Mutation
   private APP_ERROR() {
     alert('error');
+  }
+
+  private get pref() {
+    const areas: string[] = ['東京'];
+    this.items.forEach((restaurant: IRestaurant) => {
+      if (areas.indexOf(restaurant.area) === -1 && this.tokyo.indexOf(restaurant.area) === -1) {
+        areas.push(restaurant.area);
+      }
+    });
+
+    return areas;
   }
 }
 export default new Vuex.Store({
